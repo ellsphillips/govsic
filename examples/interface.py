7a781b750f2e9493b48e14cf2bed61c8f83192c0
Elliott Phillips <ellsphillipsuni@gmail.com>
2022-06-13 09:24:34 +0100

    Demo: accessing SIC properties

---
+++ examples/interface.py
@@ -0,0 +1,19 @@
+from sic import SIC
+
+
+def main() -> None:
+    sic = SIC(8110)
+
+    print(f"{sic = }")
+    
+    for label, prop in (
+        ("Code value", sic.code),
+        ("Is valid?", sic.is_valid),
+        ("Section", sic.section),
+        ("Component", sic.component),
+    ):
+        print(f"{label}:\t{prop}")
+
+
+if __name__ == "__main__":
+    main()
