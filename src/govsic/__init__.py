1791ad8b0230bdf40154739fc3024b6c30f8ced6
Elliott Phillips <ellsphillipsuni@gmail.com>
2022-07-15 14:35:48 +0100

    Feature: module-level constants access

--- src/govsic/__init__.py
+++ src/govsic/__init__.py
@@ -1,3 +1,6 @@
+from govsic.constants import (ALL_CODES, DORMANT, LETTERS, SECTIONS, Component,
+                              SectionBoundaries)
+from govsic.data.sections import SECTION
 from govsic.parser import parse
 from govsic.sic import SIC
 
